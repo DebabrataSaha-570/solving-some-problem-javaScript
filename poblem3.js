56da06f731cc18eb1b1aa33d5c721142f35b7014
Debabrata Saha <sahadebabrata570@gmail.com>
2021-08-22 20:02:44 +0600

    problem 3 solved

---
+++ poblem3.js
@@ -0,0 +1,8 @@
+// তিনটা সংখ্যা এর যোগ করতে পারবে এমন একটা ফাংশন লিখো 
+
+function addThreeNum(number1, number2, number3) {
+    var sum = number1 + number2 + number3;
+    return sum;
+}
+
+console.log(addThreeNum(30, 0.001, 3.12))
