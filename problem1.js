78ac8818b558cdf19652d940acdb83e804015827
Debabrata Saha <sahadebabrata570@gmail.com>
2021-08-22 19:25:51 +0600

    problem 1 solved

---
+++ problem1.js
@@ -0,0 +1,15 @@
+// একটা লুপ লিখতে হবে যেটা ১ থেকে ১০০ পর্যন্ত যত সংখ্যা আছে সেটা দেখাবে
+
+// for loop 
+
+for (i = 1; i <= 100; i++) {
+    console.log(i)
+}
+
+// while loop 
+
+var i = 1;
+while (i <= 100) {
+    console.log(i)
+    i++;
+}
